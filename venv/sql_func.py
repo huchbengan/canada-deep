453f5288c6acc026393762acb40c40daffe39a09
hu.chenghong <925780403@qq,com>
2022-08-31 18:53:08 +0800

    1、密码为空可以报错； 2、bugfix

--- venv/sql_func.py
+++ venv/sql_func.py
@@ -12,12 +12,16 @@ def read_sql_table(talbe_name, db_name):
     :param db_name: 库名
     :return:
     '''
-    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format('root', password, 'localhost', '3306', db_name))
-    try:
-        pd.read_sql_table(table_name=talbe_name, engine=engine)
-        print('{}{}数据读取成功'.format(talbe_name, db_name))
-    except:
-        print('读取失败')
+    if password:
+        engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format('root', password, 'localhost', '3306', db_name))
+        try:
+            df = pd.read_sql_table(table_name=talbe_name, con=engine)
+            print('库:{} 表:{}数据读取成功'.format(talbe_name, db_name))
+            return df
+        except:
+            print('读取失败')
+    else:
+        raise 'mysql密码为空'
 
 
 def write_sql_table(table_name, db_name, df: pd.DataFrame, type='replace'):
@@ -29,10 +33,19 @@ def write_sql_table(table_name, db_name, df: pd.DataFrame, type='replace'):
     :param type: 默认是先删除原表，然后写入新的数据，如果只是新增数据，则使用:append
     :return:
     '''
-    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format('root', password, 'localhost', '3306', db_name))
-    try:
-        df.to_sql(table_name, engine, index=False, if_exists=type)
-        print('{}{}数据{}成功'.format(talbe_name, db_name, type))
-    except:
-        print('写入失败')
+    if password:
+        engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format('root', password, 'localhost', '3306', db_name))
+        try:
+            df.to_sql(name=table_name, con=engine, index=False, if_exists=type)
+            print('{}{}数据{}成功'.format(table_name, db_name, type))
+        except:
+            print('写入失败')
+    else:
+        raise 'mysql密码为空'
+    # df.to_sql(name=table_name, con=engine, index=False, if_exists=type)
+    # print('{}{}数据{}成功'.format(table_name, db_name, type))
 
+
+# df = pd.read_excel('浇水分类代表物种ID.xlsx',sheet_name='全物种类群分类ID')
+df = pd.DataFrame([{'uid':1,'groupid':'cwater0002'}])
+write_sql_table(table_name='test_water',db_name='testdb',df=df)
