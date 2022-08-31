a128a8edabcae5c82d6ce1de8ffe80aae9d4da06
hu.chenghong <925780403@qq,com>
2022-08-31 14:27:12 +0800

    通用转成英文标准标题的方法

---
+++ venv/englist_title.py
@@ -0,0 +1,205 @@
+boo = """
+Summary
+Do I Need to Prune My {plant_name}?
+When Should I Prune My {plant_name}?
+What Should Be Considered While Pruning {plant_name}?
+How to Prune {plant_name}?
+What Should I Do After Pruning {plant_name}?
+Summary
+Does {plant_name} Need to be Pruned?
+When To Prune {plant_name}?
+What Tools Should I Use for Pruning {plant_name}
+How do I prune {plant_name}?
+Can You Grow {plant_name} from Cuttings?
+Are there any tips for me to prune {plant_name}?
+Summary
+Do I need to prune my {plant_name}? 
+When should/shouldn’t I prune {plant_name}?
+How do I prune {plant_name}?
+What should I look for when pruning in different seasons?
+Are there any tips for me to prune {plant_name}?
+Are there any cautions I have to be careful with?
+Summary
+Do I Need to Prune My {plant_name}?
+When Should I Prune My {plant_name}?
+How To Prune {plant_name}s? 
+What Should I Be Careful of When I Prune in Different Seasons?
+What Should I Do After Pruning My {plant_name}?
+What Tools Do I Need to Prune My {plant_name}?
+Should I Cut at An Angle When Pruning?
+Are There Any Tips for Me for Pruning My {plant_name}?
+Summary
+Do I Need to Prune My {plant_name}?
+When Should I Prune My {plant_name}?
+How Do I Prune My {plant_name}?
+What Should I Do After Pruning My {plant_name}?
+Summary
+Do I Need to Prune My {plant_name}?
+When Should I Prune My {plant_name}?
+How to Prune {plant_name}s?
+What Should I Do After Pruning My {plant_name}?
+Are There Any Tips for Me for Pruning My {plant_name}?
+Summary
+Do you need to prune your {plant_name}?
+When to prune your {plant_name}?
+How to prune {plant_name}: tools, tricks, and techniques?
+How do you prune {plant_name} during different seasons or stages of growth?
+What should I do after pruning {plant_name}? 
+What Is The Common Problem and how to fix your {plant_name} by Pruning?
+Summary
+Do I need to Prune my {plant_name}?
+When You Should Prune {plant_name} vs. When You Shouldn't?
+How to Prune {plant_name}?
+How Should I Pay Attention To Pruning {plant_name} During The Different Growth Stages?
+What To Look For When Pruning {plant_name} In Different Seasons?
+Should I Prune My {plant_name} If I Grow It Indoors?
+Are There Any Pruning Tips for My {plant_name}?
+Are There Pruning Cautions I Should Know?
+Summary
+Do I Need to Prune My {plant_name}?
+When Should I Prune My {plant_name}?
+How to Prune your {plant_name}?
+What should I do after pruning it?
+Summary
+Does {plant_name} need to be pruned?
+When to prune {plant_name}?
+Are There Any Instructions for pruning {plant_name}?
+How To Prune {plant_name} during different growth stages?
+How To Prune {plant_name} during different seasons?
+What to do after pruning {plant_name}?
+What Are Other tips and tricks for pruning {plant_name}?
+How {plant_name} drinks in the wild? 
+How to water {plant_name}? 
+How often should I water {plant_name}? 
+What should I do if {plant_name} is overwatered? 
+What should I do if {plant_name} is underwatered? 
+Summary 
+How to Water {plant_name}? 
+How Often to Water {plant_name}? 
+What should I do if I watered {plant_name} too much or too little?
+Summary
+What is the Best Way to Water {plant_name}?
+How Often Should You Water {plant_name}?
+What to Do if You Water {plant_name} Too Much or Too Little?
+What Should You Consider When Watering {plant_name} in Different Seasons and Growth Periods?
+How Should You Water {plant_name} Differently if You Grow it Indoors?
+Summary
+How Often to Water {plant_name}?
+How Much to Water {plant_name}?
+What is the Best Way to Water {plant_name}?
+How to Know if You Are Watering {plant_name} Enough?
+What to Do if Your Water {plant_name} Too Much or Too Little?
+How to Water {plant_name} through the Seasons?
+How to Water {plant_name} at Different Growth Stages?
+What's the difference between Watering {plant_name} Indoors and Outdoors?
+Summary
+How Often to Water {plant_name}?
+How Much to Water {plant_name}?
+What is the Best Way to Water the {plant_name}?
+How to Know if You Are Watering {plant_name} Enough?
+What to Do if Your Water {plant_name} Too Much or Too Little?
+How to Water {plant_name} through the Seasons?
+How to Water {plant_name} at Different Growth Stages?
+What's the difference between Watering {plant_name} Indoors and Outdoors?
+Summary
+How Often to Water {plant_name}?
+How Much to Water {plant_name}?
+What is the Best Way to Water the {plant_name}?
+How to Know if You Are Watering {plant_name} Enough?
+What to Do if Your Water {plant_name} Too Much or Too Little?
+How to Water {plant_name} Through the Seasons?
+What's the difference between Watering {plant_name} Indoors and Outdoors?
+Summary
+How Often to Water {plant_name}?
+How Much to Water {plant_name}?
+What is the Best Way to Water the {plant_name}?
+How to Know if You Are Watering {plant_name} Enough?
+What to Do if You Water {plant_name} Too Much or Too Little?
+How to Water {plant_name} through the Seasons?
+How to Water {plant_name} at Different Growth Stages?
+What's the difference between Watering {plant_name} Indoors and Outdoors?
+Summary
+How to Water {plant_name}?
+How Often to Water?
+What should I do if I water it too much or too little?
+Summary
+How Often to Water {plant_name}?
+How Much to Water {plant_name}?
+What is the Best Way to Water the {plant_name}?
+How to Know if You Are Watering {plant_name} Enough?
+What to Do if Your Water {plant_name} Too Much or Too Little?
+How to Water {plant_name} through the Seasons?
+How to Water {plant_name} at Different Growth Stages?
+What's the difference between Watering {plant_name} Indoors and Outdoors?
+Summary
+How Often to Water {plant_name}?
+How Much to Water {plant_name}?
+What is the Best Way to Water the {plant_name}?
+What to Do if Your Water {plant_name} Too Much or Too Little?
+How to Water {plant_name} through the Seasons?
+How to Water {plant_name} at Different Growth Stages?
+What's the difference between Watering {plant_name} Indoors and Outdoors?
+Summary
+Why Should I Water {plant_name}?
+How Much Do I Need to Water my {plant_name}?
+How Often Should I Water {plant_name}?
+What’s the Best Method to Water the {plant_name}?
+How to Make Sure that I Water {plant_name} Adequately?
+What should I do if I water it too much/too little?
+Should I Adjust the Watering Frequency According to Different Seasons or Climate?
+Should I change the watering frequency during different growing stages of my {plant_name}?
+What should I be careful with when I water my {plant_name} in different seasons, climates, or during different growing periods of my plants?
+Should I water {plant_name} differently when I plant it indoors but not outdoors?
+Summary
+How much/open should I water {plant_name}? 
+What will happen if I over-water {plant_name}?
+What should I do if {plant_name} under-watered?
+Watering According to Seasonal Changes
+What's the Best Way to Water {plant_name}?
+Should I water it differently when I plant it indoors but not outdoors?
+Summary
+Why Should I Water the {plant_name}?
+How Much Do I Need to Water my {plant_name}?
+How Often Should I Water my Plants?
+What's the Best Method to Water the {plant_name}?
+How to Make Sure that I Water It Adequately?
+What Should I Do if I Water It Too much/Too Little?
+Should I Adjust the Watering Frequency According to Different Seasons or Climates?
+Should I Adjust the Watering Frequency during the Different Growing Stages of my Plants?
+What should I be careful with when I water my plants in different seasons, climates, or during different growing periods of my plants?
+Should I Water it Differently when I plant it Indoors but not outdoors?
+Summary
+How to Water {plant_name}?
+How Often to Water?
+What should I do if I water it too much or too little?
+Summary
+"""
+
+stop_words = ['on', 'in', 'and', 'of', 'a', 'an', 'between',
+              'to', 'from', 'for', 'the', 'at', 'before', 'after',
+              'by', 'under', 'above', 'with', 'into', 'within',
+              'without', 'over', 'off','or']
+
+
+def foo(a_title: str):
+    original = a_title.title().strip()
+    res = []
+    for item in original.split():
+        if item.lower() in stop_words:
+            item = item.lower()
+            res.append(item)
+        else:
+            res.append(item)
+    res = ' '.join(res).strip()
+
+    if len(res) == len(original):
+        return res
+    else:
+        return '长度不一致', a_title
+
+
+xx = foo('How to Identify, Treat, And Prevent Root Rot? ')
+
+boo = [i for i in boo.split('\n') if i != '']
+for i in boo:
+    print(foo(i))
